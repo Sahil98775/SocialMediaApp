60dac0ee221436e904ab5669974a81f47bfcb632
Sahil98775 <sahil.sharma@chicmicstudios.in>
2026-02-27 18:42:33 +0530

    commitsocialApp

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { useState, useEffect, useCallback } from "react";
 
